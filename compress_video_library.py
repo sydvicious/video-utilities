1bf43cbbc997243f1f6b35026ee4dc31de2e03db
Syd Polk <syd@indeed.com>
2023-08-07 11:35:10 -0500

    Fix integer validation

--- compress_video_library.py
+++ compress_video_library.py
@@ -13,8 +13,8 @@ import argparse
 
 def check_positive(value):
     ivalue = int(value)
-    if ivalue <= 0:
-        raise argparse.ArgumentTypeError("%s is an invalid positive int value" % value)
+    if ivalue < 0:
+        raise argparse.ArgumentTypeError("%s is an invalid non-negative int value" % value)
     return ivalue
 
 parser = argparse.ArgumentParser(description="Convert video files to libx265 mp4 files using ffmpeg",
