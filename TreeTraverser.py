07323f115a234975a673ada72ea8e230431ac344
Syd Polk <syd@sydpolk.com>
2023-08-10 18:59:43 -0500

    More diagnostics

--- TreeTraverser.py
+++ TreeTraverser.py
@@ -211,8 +211,10 @@ class TreeTraverser:
                 time_of_file = datetime.datetime.fromtimestamp(path.stat().st_mtime)
 
                 if path.is_file():
-                    if path.stat().st_size > size or time_of_file > time_24_hours_ago:
-                        print(f'{video} has changed size since queue. Removing and letting the refresh put it back.')
+                    if path.stat().st_size > size:
+                        print(f'{video} has changed size since queue ({self.size_string(path.stat().st_size)} vs {self.size_string(size)}). Removing and letting the refresh put it back.')
+                    elif time_of_file > time_24_hours_ago:
+                        print(f'{video} is too new ({time_of_file}. Removing and letting the refresh put it back.')
                     elif not self.converter.convert_video(video, dest):
                         self.write_error(video)
                         print("")
