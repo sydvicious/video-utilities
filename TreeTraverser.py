c8e9ec334fe2a0bc099e25ba169e45fe2fa746ef
Syd Polk <syd@sydpolk.com>
2025-04-01 08:18:06 -0500

    Ignore Logs directory in channels

--- TreeTraverser.py
+++ TreeTraverser.py
@@ -23,7 +23,7 @@ class TreeTraverser:
 
     video_suffixes = ['.h265.mp4', '.mp4', '.mkv', '.webm', '.avi', '.ts', '.m4v',
                     '.MP4', '.mpg', '.mov', '.MOV', '.3gp', '.h265']
-    directories_to_skip = ['tmp', '.grab', 'Photos Library.photoslibrary', 'Archive', 'Ripped', 'Music', 'Recordings - raw']
+    directories_to_skip = ['tmp', '.grab', 'Photos Library.photoslibrary', 'Archive', 'Ripped', 'Music', 'Recordings - raw', 'Logs']
     file_pattern_to_skip = '.*\\(copy.*\\)'
     file_pattern_to_skip_re = re.compile(file_pattern_to_skip)
 
